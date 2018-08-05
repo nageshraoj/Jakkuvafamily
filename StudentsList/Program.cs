a7a54637f33927416d3a55f05fa3f5d4a2afca4e
Nageswara Rao Jakkuva <nageshraoj@gmail.com>
2018-08-05 21:18:44 +0530

    .

---
+++ StudentsList/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentsList
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Students studentsList = new Students();
+            strStudent nStud = new strStudent();
+            nStud.Number = 100; nStud.Name = "Tarun";nStud.Gender = sex.male;
+            studentsList.AddStudent(nStud);
+
+            nStud.Number = 101; nStud.Name = "Barath"; nStud.Gender = sex.male;
+            studentsList.AddStudent(nStud);
+
+            nStud.Number = 102; nStud.Name = "Mumar"; nStud.Gender = sex.male;
+            studentsList.AddStudent(nStud);
+
+            studentsList.ShowStudent(100);
+
+        }
+    }
+}
---
+++ StudentsList/Students.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentsList
+{
+    public struct strStudent
+    {
+        public int Number;
+        public string Name;
+        public sex Gender;
+    }
+
+    public enum sex
+    {
+        male,
+        femail
+    }
+
+   
+
+
+    public class Students
+    {
+        public List<strStudent> lstStudents = new List<strStudent>();
+       
+        public void AddStudent(strStudent nStudent)
+        {
+            lstStudents.Add(nStudent);
+        }
+
+        public void ShowStudent(int sno)
+        {
+            List<strStudent> slist = new List<strStudent>() { lstStudents.Find(x => x.Number == sno) };
+            foreach (var slst in slist)
+            {
+                Console.WriteLine(slst.Name);
+            }
+
+        }
+    }
+}
