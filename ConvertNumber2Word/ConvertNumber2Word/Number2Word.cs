6bcdf68799e4e9759bd6a43a9909b05bc872cf2d
Nageswara Rao Jakkuva <nageshraoj@gmail.com>
2018-08-07 16:49:08 +0530

    .

---
+++ ConvertNumber2Word/ConvertNumber2Word/Number2Word.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConvertNumber2Word
+{
+    public class ConvertNumber
+    {
+        
+        private static  List<string> ones = new List<string>()      
+        {   "Zero",
+            "One",
+            "Two",
+            "Three",
+            "Four",
+            "Five",
+            "Six",
+            "Seven",
+            "Eight",
+            "Nine",
+            "Ten",
+            "Eleven",
+            "Twelve",
+            "Thirteen",
+            "Fourteen",
+            "Fifteen",
+            "Sixteen",
+            "Seventeen",
+            "Eighteen",
+            "Nineteen"
+        };
+
+        private static List<string> tens = new List<string>()
+        {
+            "Zero",
+            "Ten",
+            "Twenty",
+            "Thirty",
+            "Fourty",
+            "Fifty",
+            "Sixty",
+            "Seventy",
+            "Eighty",
+            "Ninty"
+        };
+      
+       
+        public static string ToWord(long inputNumber)
+        {
+            string textValue = "";
+            int localNumber;
+          
+            if (inputNumber == 0) return "Zero";  
+            if (inputNumber < 0) return "minus " + ToWord(Math.Abs(inputNumber)); 
+
+            if ((inputNumber / 1000000) > 0)   
+                {  
+                textValue += ToWord(inputNumber / 100000) + " Lakes ";  
+                inputNumber %= 1000000;  
+                } 
+
+            if ((inputNumber / 1000) > 0)   
+                {  
+                textValue += ToWord(inputNumber / 1000) + " Thousand ";  
+                inputNumber %= 1000;  
+                } 
+
+            if ((inputNumber / 100) > 0)   
+                {  
+                textValue += ToWord(inputNumber / 100) + " Hundrad ";  
+                inputNumber %= 100;  
+                }
+
+            localNumber = (int)inputNumber;
+            if (localNumber < 20)
+            {
+                textValue += ones[localNumber];  
+            }
+            else   
+             {  
+                textValue += tens[localNumber / 10];  
+                if ((localNumber % 10) > 0) textValue += " " + ones[localNumber % 10];  
+             } 
+
+            return textValue;   
+        }
+
+
+
+    }
+}
---
+++ ConvertNumber2Word/ConvertNumber2Word/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConvertNumber2Word
+{
+    class MainClass
+    {
+       
+        public static void Main(string[] args)
+        {
+           string newNumber;
+            do
+            {
+                newNumber = "";
+            try
+            {
+                    Console.Write("Please Enter a number ");
+                    newNumber = Console.ReadLine();
+                    Console.WriteLine(ConvertNumber.ToWord(Convert.ToInt32(newNumber)));
+                      
+                }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception occered while converting " + ex.Message);
+            }
+        
+
+            } while (newNumber != "");
+
+        }
+    }
+}
