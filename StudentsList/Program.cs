ff43f73a1bff901961b0e34284a840c0b3681919
Nageswara Rao Jakkuva <nageshraoj@gmail.com>
2018-08-07 16:51:29 +0530

    Commited

--- StudentsList/Program.cs
+++ StudentsList/Program.cs
@@ -17,6 +17,9 @@ namespace StudentsList
             nStud.Number = 102; nStud.Name = "Mumar"; nStud.Gender = sex.male;
             studentsList.AddStudent(nStud);
 
+            nStud.Number = 103; nStud.Name = "Radha"; nStud.Gender = sex.femail;
+            studentsList.AddStudent(nStud);
+
             studentsList.ShowStudent(100);
 
         }
